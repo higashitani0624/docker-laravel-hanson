2380a36b3371db0f0efa7659f9dad85aac80f3eb
higashitani0624 <higashitaniyu@crunchtimer.jp>
2022-02-27 18:09:48 +0900

    feat tutorial 4 end

--- src/app/Http/Controllers/TaskController.php
+++ src/app/Http/Controllers/TaskController.php
@@ -4,16 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Folder;
+use App\Models\Task;
 
 class TaskController extends Controller
 {
     public function index(int $id)
     {
         $folders = Folder::all();
+        $current_folder = Folder::find($id);
+        $tasks = $current_folder->tasks()->get();
 
         return view('tasks/index', [
             'folders' => $folders,
-            'current_folder_id' => $id,
+            'current_folder_id' => $current_folder->id,
+            'tasks' => $tasks,
         ]);
     }
 }
